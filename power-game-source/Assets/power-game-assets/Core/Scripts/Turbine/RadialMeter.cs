eef619c0894ad0f1d87b09355eaa53041a2a549d
Christopher Robertson <christopherprobertson@gmail.com>
2021-10-30 14:26:52 +0100

    - Successfully have the Radial Arrow aiming towards the correct position at a reasonable speed.

--- power-game-source/Assets/power-game-assets/Core/Scripts/Turbine/RadialMeter.cs
+++ power-game-source/Assets/power-game-assets/Core/Scripts/Turbine/RadialMeter.cs
@@ -4,15 +4,21 @@ namespace power.turbine
 {
     public class RadialMeter : MonoBehaviour
     {
-        public float t = 0.0f;
         [SerializeField]
         private float speed = 1.25f;
         [SerializeField]
-        private Vector2 rotateBounds = new Vector2(0.0f, -180.0f);
+        private Vector2 directionBounds = new Vector2(-1.0f, 1.0f);
+
+        private float t = 0.0f;
+        private Vector3 lastDirection = Vector3.zero;
 
         [SerializeField]
         private Transform arrow = null;
-        private Vector3 lastDirection = Vector3.zero;
+        
+        private void Start()
+        {
+            SetTarget(1.0f);
+        }
 
         private void FixedUpdate()
         {
@@ -21,16 +27,18 @@ namespace power.turbine
 
         private void Rotate()
         {
-            float z = Mathf.Lerp(rotateBounds.x, rotateBounds.y, t);
-            Vector3 rot = arrow.transform.eulerAngles;
-
-            float lerpZ = Mathf.Lerp(rot.z, z, Time.deltaTime * speed);
-            arrow.transform.rotation = Quaternion.Euler(new Vector3(rot.x, rot.y, lerpZ));
+            arrow.transform.up = Vector3.Slerp(arrow.transform.up, new Vector3(GetTarget(), lastDirection.y, lastDirection.z), Time.deltaTime * speed);
         }
 
         private float GetTarget()
         {
-            return Mathf.Lerp(rotateBounds.x, rotateBounds.y, t);
+            return Mathf.Lerp(directionBounds.x, directionBounds.y, t);
+        }
+
+        private void SetTarget(float t)
+        {
+            this.t = t;
+            lastDirection = arrow.transform.up;
         }
     }
 }
