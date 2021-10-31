5cce9cfa641a73f683676c1f58e74c6b42be7272
Christopher Robertson <christopherprobertson@gmail.com>
2021-10-31 11:41:56 +0000

    - Introduced some randomness through the speed and the change of speed in the RotateTurbine.

--- power-game-source/Assets/power-game-assets/Core/Scripts/Turbine/RotateTurbine.cs
+++ power-game-source/Assets/power-game-assets/Core/Scripts/Turbine/RotateTurbine.cs
@@ -8,7 +8,15 @@ namespace power.turbine
         private Vector2 speed = new Vector2(0, 10.0f);
         private float currentSpeed = 0.0f;
         [SerializeField]
+        private Vector2 increaseSpeedRange = new Vector2(0.01f, 0.05f);
+        [SerializeField]
         private float increaseSpeed = 0.01f;
+
+        [SerializeField]
+        private Vector2 speedChangeTimeRange = new Vector2(2.0f, 10.0f);
+        [SerializeField]
+        private float timeToChangeSpeed = 2.5f;
+
         [Range(0, 1)]
         public float t = 0.5f;
 
@@ -22,8 +30,17 @@ namespace power.turbine
         [SerializeField]
         private bool invertDirection = true;
 
+        private void Start()
+        {
+            Randomise();
+        }
+
         private void FixedUpdate()
         {
+            timeToChangeSpeed -= Time.deltaTime;
+            if (timeToChangeSpeed <= 0)
+                Randomise();
+
             if (graduallyIncreaseSpeed)
                 t += increaseSpeed * Time.deltaTime;
 
@@ -33,6 +50,12 @@ namespace power.turbine
 
             if (canRotate)
                 Rotate();
+        }
+
+        private void Randomise()
+        {
+            increaseSpeed = GetRandomIncreaseSpeed();
+            timeToChangeSpeed = GetRandomTime();
         }
 
         private void Rotate()
@@ -46,6 +69,16 @@ namespace power.turbine
         private void SetSpeed(float t)
         {
             currentSpeed = Mathf.Lerp(speed.x, speed.y, t);
+        }
+
+        private float GetRandomIncreaseSpeed()
+        {
+            return Random.Range(increaseSpeedRange.x, increaseSpeedRange.y);
+        }
+
+        private float GetRandomTime()
+        {
+            return Random.Range(speedChangeTimeRange.x, speedChangeTimeRange.y);
         }
     }
 }
