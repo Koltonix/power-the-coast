ee7a0728be39c44425b3c4a0001570d94af77040
Christopher Robertson <christopherprobertson@gmail.com>
2021-10-31 21:19:20 +0000

    - Added a variety of common appliances.

--- power-game-source/Assets/power-game-assets/Core/Scripts/UI/Highscore.cs
+++ power-game-source/Assets/power-game-assets/Core/Scripts/UI/Highscore.cs
@@ -1,13 +1,25 @@
+using System;
 using UnityEngine;
 using TMPro;
 using power.data;
 
 namespace power.utilities
 {
+    // Source: https://www.cse.org.uk/advice/advice-and-support/how-much-electricity-am-i-using
+    [Serializable]
+    public struct CommonAppliance
+    {
+        public string name;
+        public int powerRating;
+    }
+
     public class Highscore : MonoBehaviour
     {
         [SerializeField]
         private PowerData data = null;
+
+        [SerializeField]
+        private CommonAppliance[] appliances = null;
 
         [SerializeField]
         private GameObject newHighScoreUI = null;
@@ -28,6 +40,20 @@ namespace power.utilities
 
                 newHighScoreUI.SetActive(true);
             }
+
+            CommonAppliance appliance = appliances[UnityEngine.Random.Range(0, appliances.Length)];
+
+            scoreText.text = Math.Round(MWToAppliance(score, appliance.powerRating), 2) +  "s " + appliance.name + " for an hour"; 
+            highscoreText.text = highscore + "MW";
+        }
+
+        // 800-1500 Watts for a Toaster = https://www.cse.org.uk/advice/advice-and-support/how-much-electricity-am-i-using
+        private float MWToAppliance(float mw, int rating)
+        {
+            float kw = mw * 1000;
+            float w = kw * 1000;
+
+            return w / rating;
         }
     }
 }
